d0b1ade45efacddb84098b3c3ca893d42da83116
DESKTOP-IHKQ5T5\locan <lo.cannella@gmail.com>
2021-06-05 11:18:44 +0200

    Inizializzazione progetto

---
+++ museo-siw/src/main/java/it/uniroma3/siw/spring/MuseoSiwApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseoSiwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseoSiwApplication.class, args);
+	}
+
+}
